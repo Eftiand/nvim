a04bc19904a2a1a609de16aade53552a720b360d
Kristian Jeremic <kristianjer@gmail.com>
2024-09-11 01:01:10 +0200

    noice update

--- lua/meon/plugins/noice.lua
+++ lua/meon/plugins/noice.lua
@@ -2,12 +2,6 @@ return {
 	"folke/noice.nvim",
 	event = "VeryLazy",
 	opts = {
-		cmdline = {
-			enabled = true, -- Keep the enhanced command-line UI
-		},
-		popupmenu = {
-			enabled = true, -- Keep the enhanced search UI
-		},
 		messages = {
 			enabled = false, -- Disable general message notifications
 		},
@@ -15,37 +9,11 @@ return {
 			progress = {
 				enabled = false, -- Disable LSP progress messages
 			},
-			hover = {
-				enabled = true, -- Keep LSP hover features if needed
-			},
 			message = {
 				enabled = false, -- Keep LSP signature help if needed
 			},
-			signature = {
-				enabled = true, -- Keep LSP signature help if needed
-			},
 		},
-		routes = {
-			-- Suppress all types of LSP messages from csharp_ls
-			{
-				filter = {
-					event = "lsp",
-					cond = function(message)
-						return message.client and message.client.name == "csharp_ls"
-					end,
-				},
-				opts = { skip = true }, -- Skip all messages from csharp_ls
-			},
-			{
-				filter = {
-					event = "notify", -- Filter out notifications
-					cond = function(message)
-						return message.client and message.client.name == "csharp_ls"
-					end,
-				},
-				opts = { skip = true }, -- Skip notifications from csharp_ls
-			},
-		},
+		routes = {},
 	},
 	dependencies = {
 		"MunifTanjim/nui.nvim", -- Required dependency for Noice's UI enhancements
