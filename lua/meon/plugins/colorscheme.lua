eb518645b40375529eb1869f3fcbbd071a4138fb
Kristian Jeremic <kristianjer@gmail.com>
2024-09-17 20:13:46 +0200

    Update colroschem

--- lua/meon/plugins/colorscheme.lua
+++ lua/meon/plugins/colorscheme.lua
@@ -4,6 +4,7 @@ return {
 	config = function()
 		require("onedark").setup({
 			style = "warmer",
+			transparent = true,
 			colors = {
 				bg0 = "#181A1F",
 				purple = "#D55FDE",
@@ -12,14 +13,24 @@ return {
 				green = "#89CA78",
 				cyan = "#58C1CF",
 				red = "#EF596F",
+				orange = "#F58838",
+				dark_yellow = "#D19A66",
 
 				--red = "#D19A66",
 				--cyan = "#DE5D68",
 			},
 			highlights = {
 				["@variable.member"] = { fg = "$red" },
+				["@variable.parameter"] = { fg = "$dark_yellow" },
 				["@property"] = { fg = "$red" },
+				-- ["@constuctor"] = { fmt= "" },
 				["@lsp.type.property"] = { fg = "$red" },
+				["@lsp.type.parameter"] = { fg = "$dark_yellow" },
+				["@lsp.type.interface"] = { fg = "$orange" },
+				["@keyword.modifier.c_sharp"] = { fg = "$purple" },
+				["@keyword.type.c_sharp"] = { fg = "$purple" },
+				["@type.builtin"] = { fg = "$purple" },
+				["@constant.builtin"] = { fg = "$purple" },
 			},
 		})
 		vim.cmd("colorscheme onedark")
