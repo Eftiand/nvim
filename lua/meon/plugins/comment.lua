e91287ed2057fe61bef43ea57f2098d50551291f
kristian.jeremic <kristian.jeremic@neovici.se>
2024-11-07 15:32:54 +0100

    update

--- lua/meon/plugins/comment.lua
+++ lua/meon/plugins/comment.lua
@@ -1,19 +1,30 @@
 return {
-  "numToStr/Comment.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    "JoosepAlviste/nvim-ts-context-commentstring",
-  },
-  config = function()
-    -- import comment plugin safely
-    local comment = require("Comment")
+	"numToStr/Comment.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"JoosepAlviste/nvim-ts-context-commentstring",
+	},
+	config = function()
+		-- import comment plugin safely
+		local comment = require("Comment")
+		local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+		-- enable comment
+		comment.setup({
+			-- for commenting tsx, jsx, svelte, html files
+			pre_hook = ts_context_commentstring.create_pre_hook(),
 
-    local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
-
-    -- enable comment
-    comment.setup({
-      -- for commenting tsx, jsx, svelte, html files
-      pre_hook = ts_context_commentstring.create_pre_hook(),
-    })
-  end,
+			-- Additional configuration for C# and HTML
+			ft = {
+				cs = {
+					-- C# uses // for single line comments and /* */ for multi-line comments
+					__default = "// %s",
+					__multiline = "/* %s */",
+				},
+				html = {
+					-- HTML uses <!-- --> for comments
+					__default = "<!-- %s -->",
+				},
+			},
+		})
+	end,
 }
