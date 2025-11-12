d963c304c1d633796234acfa7523c3d6af93fc69
kristian.jeremic <kristianjer@gmail.com>
2025-11-12 14:45:19 +0100

    remove telescope

--- lua/meon/plugins/nvim-tree.lua
+++ lua/meon/plugins/nvim-tree.lua
@@ -53,5 +53,6 @@ return {
     keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
     keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
     keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+    keymap.set("n", "<leader>se", "<cmd>NvimTreeFindFile<CR>", { desc = "Show current file in explorer" }) -- show current file in explorer
   end
 }
