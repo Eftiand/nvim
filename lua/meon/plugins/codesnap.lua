cdbe615986c8764ea44e13ad6ddf64e70baec3b6
kristian.jeremic <kristianjer@gmail.com>
2025-12-09 15:27:47 +0100

    update codenspa and autosession
    asd

--- lua/meon/plugins/codesnap.lua
+++ lua/meon/plugins/codesnap.lua
@@ -1,13 +1,14 @@
 return {
   "mistricky/codesnap.nvim",
+  tag = "v2.0.0-beta.17",
   lazy = true,
   keys = {
     { "<leader>cc", "<cmd>CodeSnap<cr>", mode = "x", desc = "Save selected code snapshot into clipboard" },
   },
   opts = {
-    has_breadcrumbs = false,
-    bg_color = "#535c68",
-    bg_x_padding = 50,
-    bg_y_padding = 30,
+    -- has_breadcrumbs = false,
+    -- bg_color = "#535c68",
+    -- bg_x_padding = 50,
+    -- bg_y_padding = 30,
   },
 }
