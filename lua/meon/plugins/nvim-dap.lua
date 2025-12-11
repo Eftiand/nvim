a9a132ac88b4e29bb6f2da1a0c42ffae453039e8
kristian.jeremic <kristianjer@gmail.com>
2025-12-11 08:55:33 +0100

    move nvim-dap to utils folder

--- lua/meon/plugins/nvim-dap.lua
+++ lua/meon/plugins/nvim-dap.lua
@@ -14,30 +14,7 @@ return {
 
     -- Load configurations from .vscode/launch.json (with JSON5 support)
     local vscode = require("dap.ext.vscode")
-    vscode.json_decode = function(content)
-      -- Remove // comments only outside of strings
-      local result, in_string, i = {}, false, 1
-      while i <= #content do
-        local c = content:sub(i, i)
-        if c == '"' and content:sub(i - 1, i - 1) ~= "\\" then
-          in_string = not in_string
-          table.insert(result, c)
-        elseif not in_string and content:sub(i, i + 1) == "//" then
-          while i <= #content and content:sub(i, i) ~= "\n" do
-            i = i + 1
-          end
-        else
-          table.insert(result, c)
-        end
-        i = i + 1
-      end
-      content = table.concat(result)
-      -- Remove trailing commas
-      while content:find(",(%s*[%]%}])") do
-        content = content:gsub(",(%s*[%]%}])", "%1")
-      end
-      return vim.json.decode(content)
-    end
+    vscode.json_decode = require("meon.util.json5").decode
     vscode.load_launchjs()
 
     vim.keymap.set("n", "<F5>", dap.continue)
