5dfe04da3a48326e5656dbdd18b7c385c1da82e2
kristian.jeremic <kristianjer@gmail.com>
2025-12-11 09:28:14 +0100

    improve roslyn-startup

--- lua/meon/plugins/lsp/roslyn.lua
+++ lua/meon/plugins/lsp/roslyn.lua
@@ -3,14 +3,28 @@ return {
     ft = "cs",
     ---@module 'roslyn.config'
     ---@type RoslynNvimConfig
---    opts = {
---        config = {
---            settings = {
---                ["csharp|background_analysis"] = {
---                    dotnet_analyzer_diagnostics_scope = "fullSolution",
---                    dotnet_compiler_diagnostics_scope = "fullSolution",
---                },
---            },
---        },
---    },
+    opts = {
+        config = {
+            settings = {
+                ["csharp|background_analysis"] = {
+                    dotnet_analyzer_diagnostics_scope = "openFiles",
+                    dotnet_compiler_diagnostics_scope = "openFiles",
+                },
+                ["csharp|inlay_hints"] = {
+                    csharp_enable_inlay_hints_for_implicit_object_creation = false,
+                    csharp_enable_inlay_hints_for_implicit_variable_types = false,
+                    csharp_enable_inlay_hints_for_lambda_parameter_types = false,
+                    csharp_enable_inlay_hints_for_types = false,
+                    dotnet_enable_inlay_hints_for_indexer_parameters = false,
+                    dotnet_enable_inlay_hints_for_literal_parameters = false,
+                    dotnet_enable_inlay_hints_for_object_creation_parameters = false,
+                    dotnet_enable_inlay_hints_for_other_parameters = false,
+                    dotnet_enable_inlay_hints_for_parameters = false,
+                    dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+                    dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+                    dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+                },
+            },
+        },
+    },
 }
