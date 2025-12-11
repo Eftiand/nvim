0608f07ee31582279d7546a51868fbf4078490b2
kristian.jeremic <kristianjer@gmail.com>
2025-12-11 08:54:24 +0100

    update nvim-dap

--- lua/meon/plugins/nvim-dap.lua
+++ lua/meon/plugins/nvim-dap.lua
@@ -15,9 +15,26 @@ return {
     -- Load configurations from .vscode/launch.json (with JSON5 support)
     local vscode = require("dap.ext.vscode")
     vscode.json_decode = function(content)
-      content = content:gsub("//[^\n]*", "") -- remove // comments
+      -- Remove // comments only outside of strings
+      local result, in_string, i = {}, false, 1
+      while i <= #content do
+        local c = content:sub(i, i)
+        if c == '"' and content:sub(i - 1, i - 1) ~= "\\" then
+          in_string = not in_string
+          table.insert(result, c)
+        elseif not in_string and content:sub(i, i + 1) == "//" then
+          while i <= #content and content:sub(i, i) ~= "\n" do
+            i = i + 1
+          end
+        else
+          table.insert(result, c)
+        end
+        i = i + 1
+      end
+      content = table.concat(result)
+      -- Remove trailing commas
       while content:find(",(%s*[%]%}])") do
-        content = content:gsub(",(%s*[%]%}])", "%1") -- remove trailing commas
+        content = content:gsub(",(%s*[%]%}])", "%1")
       end
       return vim.json.decode(content)
     end
