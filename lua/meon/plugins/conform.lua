f66b26ceae0e7665e4d7a3edae1dde1b6eee4d05
kristian.jeremic <kristianjer@gmail.com>
2025-12-18 11:38:31 +0100

    update harpoon and coform

--- lua/meon/plugins/conform.lua
+++ lua/meon/plugins/conform.lua
@@ -1,7 +1,10 @@
 return {
 	"stevearc/conform.nvim",
-	event = { "BufWritePre" },
+	event = { "BufWritePre", "BufReadPost" },
 	cmd = { "ConformInfo" },
+	init = function()
+		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+	end,
 	keys = {
 		{
 			"<leader>fd",
@@ -9,6 +12,21 @@ return {
 				require("conform").format({ async = true, lsp_fallback = true })
 			end,
 			desc = "Format current buffer",
+		},
+		{
+			"=",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true }, function(err)
+					if not err then
+						local mode = vim.api.nvim_get_mode().mode
+						if vim.startswith(string.lower(mode), "v") then
+							vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", true)
+						end
+					end
+				end)
+			end,
+			mode = "v",
+			desc = "Format selection",
 		},
 	},
 	opts = {
