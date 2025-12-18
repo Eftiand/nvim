50e86ade2f3b2256cb1047b929c405b90884aa53
kristian.jeremic <kristianjer@gmail.com>
2025-12-18 11:42:06 +0100

    add harpoon and leap adjustment

--- lua/meon/plugins/leap.lua
+++ lua/meon/plugins/leap.lua
@@ -4,10 +4,14 @@ return {
 		config = function()
 			local leap = require("leap")
 			vim.keymap.set("n", "f", function()
-				leap.leap({ target_windows = { vim.api.nvim_get_current_win() } })
+				leap.leap({ target_windows = vim.tbl_filter(function(win)
+					return vim.api.nvim_win_get_config(win).focusable
+				end, vim.api.nvim_tabpage_list_wins(0)) })
 			end, { desc = "Leap forward" })
 			vim.keymap.set("n", "F", function()
-				leap.leap({ backward = true, target_windows = { vim.api.nvim_get_current_win() } })
+				leap.leap({ backward = true, target_windows = vim.tbl_filter(function(win)
+					return vim.api.nvim_win_get_config(win).focusable
+				end, vim.api.nvim_tabpage_list_wins(0)) })
 			end, { desc = "Leap backward" })
 		end,
 	},
