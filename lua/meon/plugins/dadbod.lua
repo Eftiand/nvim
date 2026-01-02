956702241d0719848a05b08068fb81cdfa774364
kristian.jeremic <kristianjer@gmail.com>
2026-01-02 09:00:19 +0100

    fix sql dadbod

--- lua/meon/plugins/dadbod.lua
+++ lua/meon/plugins/dadbod.lua
@@ -15,6 +15,38 @@ return {
 		{ "<leader>sf", "<Cmd>DBUIFindBuffer<CR>", desc = "Find DB buffer" },
 		{ "<leader>sa", "<Cmd>DBUIAddConnection<CR>", desc = "Add DB connection" },
 		{ "<leader>sl", "<Cmd>DBUILastQueryInfo<CR>", desc = "Last query info" },
+		{
+			"<leader>sj",
+			function()
+				-- Get the content under cursor or current line
+				local content = vim.fn.expand("<cWORD>")
+				if content == "" then
+					content = vim.api.nvim_get_current_line()
+				end
+
+				-- Try to find JSON object/array in the content
+				local json_start = content:find("[{%[]")
+				if json_start then
+					content = content:sub(json_start)
+				end
+
+				-- Open in vertical split
+				vim.cmd("vnew")
+				vim.bo.buftype = "nofile"
+				vim.bo.bufhidden = "wipe"
+				vim.bo.filetype = "json"
+
+				-- Insert content
+				vim.api.nvim_buf_set_lines(0, 0, -1, false, { content })
+
+				-- Try to format with jq if available
+				if vim.fn.executable("jq") == 1 then
+					vim.cmd("silent! %!jq .")
+				end
+			end,
+			desc = "Open JSON in buffer",
+			ft = "dbout",
+		},
 	},
 	init = function()
 		-- Use nerd fonts icons
